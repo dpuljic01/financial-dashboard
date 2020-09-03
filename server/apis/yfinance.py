72e273cad6032b282ee0d37aa389b036fee59762
Domagoj Puljić <dpuljic01@gmail.com>
2020-09-03 03:52:10 +0200

    use yfinance fork because of the bug in original package

--- server/apis/yfinance.py
+++ server/apis/yfinance.py
@@ -2,9 +2,7 @@ import yfinance as yf
 import json
 import datetime
 from server.extensions import db
-# from server.models import StockHistory
 from server.models import Stock
-from server.apis.iex import IEXFinance
 import pandas_datareader as pdr
 import pandas as pd
 
@@ -35,30 +33,6 @@ def fetch_stock_history(tickers, period="1y", interval="1d", start=None, end=Non
                 pass
 
     return res
-
-    # probably delete this below
-    # objects = []
-    # stock = create_stock(ticker)
-    # for idx_h, row_h in history.iterrows():
-    #     stock_history = StockHistory(
-    #         stock_id=stock["id"],
-    #         date=idx_h,
-    #         close=row_h["Close"],
-    #         open=row_h["Open"],
-    #         high=row_h["High"],
-    #         low=row_h["Low"],
-    #         dividends=row_h["Dividends"],
-    #         volume=row_h["Volume"]
-    #     )
-    #     objects.append(stock_history)
-    #
-    # try:
-    #     db.session.bulk_save_objects(objects)
-    #     db.session.commit()
-    # except Exception as e:
-    #     print(f"Error: {e}")
-    #     return
-    # print("Done!")
 
 
 def create_stock(ticker):
