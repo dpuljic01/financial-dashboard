4e0ff32b8a9060d1f6479ff20da7b4ca08351113
Domagoj Puljić <dpuljic01@gmail.com>
2020-09-26 01:46:37 +0200

    fix news fetch from nasdaq

--- server/apis/nasdaq.py
+++ server/apis/nasdaq.py
@@ -15,6 +15,9 @@ class NasdaqUrl:
             raise Exception("Symbol is required")
         return self.make(f"/api/quote/{symbol.upper()}/info")
 
+    def news(self, symbol):
+        return self.make(f"/api/v1/news-headlines-fetcher/{symbol.upper()}/0/4")  # first four
+
 
 class NasdaqApi(BaseApi):
     def __init__(self):
@@ -40,6 +43,15 @@ class NasdaqApi(BaseApi):
         resp = self.get(self.url.info(symbol), params=params)
         resp.raise_for_status()
         return resp.json()
+
+    def get_headlines(self, symbol):
+        url = self.url.news(symbol).replace("api", "www", 1)
+        resp = self.get(url, headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+            "Referer": f"https://www.nasdaq.com/market-activity/stocks/{symbol.lower()}/press-releases",
+        })
+        resp.raise_for_status()
+        return resp
 
 
 Nasdaq = NasdaqApi()
